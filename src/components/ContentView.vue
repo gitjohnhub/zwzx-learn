a3fc150c086ed1980b20a57b626a5b6e34fc4c59
Yi <guiyc@outlook.com>
2023-04-03 15:42:41 +0800

    fix image

--- src/components/ContentView.vue
+++ src/components/ContentView.vue
@@ -7,16 +7,20 @@
   <a-timeline>
       <a-timeline-item v-for="(item,index) in bangban_content[value1]['content']">{{index}}、{{ item }}</a-timeline-item>
     </a-timeline>
-    <a-image v-for="(img_src,index) in bangban_content[value1]['imgsrc']" :width="200" :src="img_src"></a-image>
+    <a-image v-for="(img_src,index) in img_list" :width="200" :src="img_src"></a-image>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
 import {bangban_content} from '@/utils/bangban_content'
 import bb1_1 from '@/assets/bb1_1.jpg'
-const getImageUrl = () => {
-        	// 里面可以根据需求写逻辑
-          for
-            return new URL('../assets/img/home/container_bg.png', import.meta.url).href;
-        };
+const img_list = ref([])
+const getImageUrl = () =>{
+  for(let imgsrc of  bangban_content[value1.value]['imgsrc']){
+    const img_url= new URL('../assets/'+imgsrc+'.jpg', import.meta.url).href
+    img_list.value.push(img_url)
+  }
+  return img_list
+
+  };
 const value1 = ref<string>('电子营业执照申请');
 </script>
