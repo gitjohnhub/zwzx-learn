b678f99f0359f497fa59fb86eb6fa4666dfca7d1
Yi <guiyc@outlook.com>
2023-04-06 08:33:26 +0800

    add .

---
+++ src/components/LoginView.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="login">
+  <label>
+  Username:
+  <input type="text" v-model="formData.username">
+  </label>
+  <br>
+  <label>
+  Password:
+  <input type="password" v-model="formData.password">
+  </label>
+  <br>
+  <button type="submit">Login</button>
+  </form>
+  </template>
+
+  <script lang="ts" setup>
+const formData = {
+  username: '',
+  password: '',
+  }
+const login = (()=>{
+  // Perform login logic and store user information in local storage
+  localStorage.setItem('user', JSON.stringify({ username: formData.username }))
+  // Redirect to homepage
+  this.$router.push('/homepage')
+  }
+)
+
+</script>
