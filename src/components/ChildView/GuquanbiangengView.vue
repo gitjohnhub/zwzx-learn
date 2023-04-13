3f7209f6ceeaafabe2dc4ec6bd7d7405b289a47e
yi <guiyc@outlook.com>
2023-04-13 13:47:07 +0800

    fix 股权

--- src/components/ChildView/GuquanbiangengView.vue
+++ src/components/ChildView/GuquanbiangengView.vue
@@ -13,7 +13,8 @@
     </div> -->
 
     <template v-if="!show_single_gudong">
-      <a-form ref="formRef" :model="formState_hezi">
+      <!-- 添加原股东 -->
+      <a-form :model="formState_hezi">
         <a-row v-for="(gudong, index) in formState_hezi.gudongs" :key="index">
           <a-form-item>
             <a-input
@@ -39,9 +40,80 @@
             />
           </a-form-item>
         </a-row>
+        <a-form-item> <a-button @click="addGudong">添加原股东</a-button></a-form-item>
       </a-form>
-
-      <a-button @click="addGudong">添加原股东</a-button>
+      <!-- 添加现有股东 -->
+      <a-form :model="modify_hezi">
+        <a-row v-for="(gudong, index) in modify_hezi.gudongs" :key="index">
+          <a-form-item>
+            <a-input
+              addon-before="现出资人姓名"
+              v-model:value="gudong.name"
+              placeholder="placeholder"
+            ></a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-input
+              addon-before="现出资额"
+              v-model:value="gudong.chuzie"
+              placeholder="placeholder"
+              addon-after="万元"
+            ></a-input>
+          </a-form-item>
+          <a-form-item>
+            <minus-circle-outlined
+              v-if="modify_hezi.gudongs.length > 1"
+              class="dynamic-delete-button"
+              :disabled="modify_hezi.gudongs.length === 1"
+              @click="removeModifyGudong(gudong)"
+            />
+          </a-form-item>
+        </a-row>
+        <a-form-item> <a-button @click="addModifyGudong">添加现有股东</a-button></a-form-item>
+      </a-form>
+      <!-- 添加转让信息 -->
+      <a-form :model="zhuanrang_info">
+        <a-row v-for="(zhuanrang, index) in zhuanrang_info.zhuanrangs" :key="index">
+          <!-- 转让人 -->
+          <a-form-item>
+            转让人姓名：<a-select v-model:value="zhuanrang.zhuanrang_name" style="width: 120px">
+              <a-select-option
+                v-for="(gudong, index) in formState_hezi.gudongs"
+                :key="index"
+                :value="gudong.name"
+                >{{ gudong.name }}</a-select-option
+              ></a-select
+            >
+          </a-form-item>
+          <a-form-item>
+            受让人姓名：<a-select v-model:value="zhuanrang.shourang_name" style="width: 120px">
+              <a-select-option
+                v-for="(gudong, index) in modify_hezi.gudongs"
+                :key="index"
+                :value="gudong.name"
+                >{{ gudong.name }}</a-select-option
+              ></a-select
+            >
+          </a-form-item>
+          <a-form-item>
+            <a-input
+              addon-before="转让额"
+              v-model:value="zhuanrang.zhuanrange"
+              placeholder="转让额"
+              addon-after="万元"
+            ></a-input>
+          </a-form-item>
+          <a-form-item>
+            <minus-circle-outlined
+              v-if="zhuanrang_info.zhuanrangs.length > 1"
+              class="dynamic-delete-button"
+              :disabled="zhuanrang_info.zhuanrangs.length === 1"
+              @click="removeZhuanrangInfo(zhuanrang)"
+            />
+          </a-form-item>
+        </a-row>
+        <a-form-item> <a-button @click="addZhuanrangInfo">添加转让信息</a-button></a-form-item>
+      </a-form>
     </template>
 
     <div v-if="show_single_gudong">
@@ -90,6 +162,9 @@
     <!-- 生成按钮-->
     <p>**生成内容仅供参考，下载完成后可根据实际情况修改word文档内容</p>
     <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
+      <!-- <a-button @click="testButton">测试打印</a-button> -->
+    </a-form-item>
+    <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
       <a-button type="primary" @click="generate_content"
         >生成股权转让协议、章程修正案、股东决定</a-button
       >
@@ -101,7 +176,6 @@
 import { MinusCircleOutlined } from '@ant-design/icons-vue';
 import { ref, watch, reactive } from 'vue';
 import type { FormInstance } from 'ant-design-vue';
-import HeziView from '@/components/ChildView/HeziView.vue';
 import {
   generate_download_link,
   formatDate,
@@ -112,26 +186,39 @@ import {
   add_title,
 } from '@/utils/utils';
 import locale from 'ant-design-vue/es/date-picker/locale/zh_CN';
+import { func } from 'vue-types';
 
 interface Gudong {
   name: string;
   chuzie: string;
+}
+interface Zhuanrang {
+  zhuanrang_name: string;
+  shourang_name: string;
+  zhuanrange: string;
 }
 const labelCol = { style: { width: '200px' } };
 const wrapperCol = { span: 14 };
 const company_category_list = ref(['一人有限公司', '合资有限公司']);
 const show_single_gudong = ref(true);
 const show_decide_date = ref(true);
-const show_gudong_num = ref(true);
-const show_gudong_name = ref(true);
-const show_gudong_money = ref(true);
 const formRef = ref<FormInstance>();
 const formState_hezi = reactive<{ gudongs: Gudong[] }>({
   gudongs: [],
 });
-({
+const modify_hezi = reactive<{ gudongs: Gudong[] }>({
   gudongs: [],
 });
+const zhuanrang_info = reactive<{ zhuanrangs: Zhuanrang[] }>({
+  zhuanrangs: [],
+});
+// watch(formState_hezi,(newValue,oldValue)=>{
+//   modify_hezi.value.gudongs = formState_hezi.gudongs
+//   console.log("modify_hezi=>",modify_hezi.value)
+// })
+const testButton = () => {
+  console.log('testButton=>', formState_hezi);
+};
 const addGudong = () => {
   console.log(formState_hezi.gudongs);
   formState_hezi.gudongs.push({
@@ -139,6 +226,35 @@ const addGudong = () => {
     chuzie: '',
   });
 };
+const addModifyGudong = () => {
+  console.log(modify_hezi.gudongs);
+  modify_hezi.gudongs.push({
+    name: '',
+    chuzie: '',
+  });
+};
+const addZhuanrangInfo = () => {
+  console.log(zhuanrang_info.zhuanrangs);
+  zhuanrang_info.zhuanrangs.push({
+    zhuanrang_name: '',
+    shourang_name: '',
+    zhuanrange: '',
+  });
+};
+
+const removeModifyGudong = (item: Gudong) => {
+  let index = modify_hezi.gudongs.indexOf(item);
+  if (index !== -1) {
+    modify_hezi.gudongs.splice(index, 1);
+  }
+};
+const removeZhuanrangInfo = (item: Zhuanrang) => {
+  let index = zhuanrang_info.zhuanrangs.indexOf(item);
+  if (index !== -1) {
+    zhuanrang_info.zhuanrangs.splice(index, 1);
+  }
+};
+
 const removeGudong = (item: Gudong) => {
   let index = formState_hezi.gudongs.indexOf(item);
   if (index !== -1) {
@@ -163,11 +279,10 @@ const formState = reactive({
 });
 watch(formState, (newValue, oldValue) => {
   if (formState.company_category == '合资有限公司') {
-    show_single_gudong.value = false
+    show_single_gudong.value = false;
     // show_gudong_num.value = true
-  }else {
-    show_single_gudong.value = true
-
+  } else {
+    show_single_gudong.value = true;
   }
 });
 watch(
@@ -186,11 +301,17 @@ function generate_content() {
     zhangchengxiuzhengan = generate_zhangchengxiuzhengan();
   }
   console.log(gudongjueding_content);
+  if (formState.company_category == '合资有限公司'){
+    gudongjueding_content = generate_multi_guquanzhuanrang_jueyi()
+    zhangchengxiuzhengan = generate_hezi_zhangchengxiuzhengan()
+    guquanzhuanrangxieyi_content = generate_hezi_guquanzhuanrang_xieyi()
+  }
+
 
   generate_download_link(gudongjueding_content, formState.company_name + '股东决定');
   generate_download_link(zhangchengxiuzhengan, formState.company_name + '章程修正案');
   generate_download_link(guquanzhuanrangxieyi_content, formState.company_name + '股权转让协议');
-}
+ }
 
 //一人有限公司股东决定：股权转让
 function generate_guquanzhuanrang_gudongjueding() {
@@ -215,6 +336,150 @@ function generate_guquanzhuanrang_gudongjueding() {
     add_date()
   );
 }
+// 合资有限公司股权转让
+function generate_multi_guquanzhuanrang_jueyi() {
+  const all_content =
+    add_title(`股东会决议`) +
+    add_p(
+      `根据《公司法》及本公司章程的有关规定，${formState.company_name}临时股东会会议于${formatDate(formState.decide_date)}在上海召开。本次会议由${formState_hezi.gudongs[0].name}提议召开，${formState_hezi.gudongs[0].name}于会议召开3日以前以电话方式通知全体股东，应到会股东 ${formState_hezi.gudongs.length}人，实际到会股东${formState_hezi.gudongs.length}人，占总股数100%。会议由 ${formState_hezi.gudongs[0].name}主持，形成决议如下：`
+    ) +
+    add_p(`一、${generate_tongyi()}其他股东放弃优先购买权。`) +
+    add_p(`二、股权转让后，公司股东持股情况如下：`) +
+    generate_new_chuzibili() +
+    add_p(`三、通过公司章程修正案；`) +
+    add_p(`四、公司股东发生变动后，公司董事、监事、高管人员不变。`) +
+    add_p(`.`) +
+    add_p(`以上事项表决结果：同意的，占总股数100%,`) +
+    add_p(`不同意的，占总股数 ，0%,`) +
+    add_p(`弃权的，占总股数 ，0%,`) +
+    add_qianzi('股东') +
+    add_date();
+  return all_content
+}
+// 合资有限公司 章程修正案
+function generate_hezi_zhangchengxiuzhengan() {
+  const all_content =
+    add_title(`${formState.company_name}章程修正案`) +
+    add_p('根据《公司法》及股东会决议，对章程如下：') +
+    add_p(`一、章程第四章第五条原为：${get_xiuzhengan_tiaokuan()}`) +
+    add_p(`现将该条修改为${get_xiuzhengan_modify_tiaokuan()}`)+
+    add_p(`三、章程其他条款不变。`) +
+    add_qianzi('股东') +
+    add_date();
+  return all_content;
+}
+function get_xiuzhengan_tiaokuan(){
+  let content = ''
+  for (let item of formState_hezi.gudongs) {
+    content += `股东姓名（名称）:${item.name}，出资额:${
+        item.chuzie
+      }，出资方式：${add_underLine('待修改')},出资时间:${add_underLine('待修改')}。`
+
+  }
+  return content
+}
+function get_xiuzhengan_modify_tiaokuan(){
+  let content = ''
+  for (let item of modify_hezi.gudongs) {
+    content += `股东姓名（名称）:${item.name}，出资额:${
+        item.chuzie
+      }，出资方式：${add_underLine('待修改')},出资时间:${add_underLine('待修改')}。`
+
+  }
+  return content
+}
+//合资有限公司 股权转让协议
+function generate_hezi_guquanzhuanrang_xieyi() {
+  const all_content =
+    add_title('股权转让协议') +
+    add_p(`本协议由以下各方于${add_underLine(formatDate(formState.decide_date))}在上海共同签署。`) +
+    get_zhuanrang_shourang_content().content+
+    add_p(
+      add_underLine(formState.company_name) +
+        '（以下称标的公司）注册资本' +
+      add_underLine(`${get_zhuceziben()}`) +
+        `万元人民币，${get_zhuanrang_shourang_content().oneLine}`+'根据有关法律、法规规定，经本协议各方友好协商，达成条款如下：'
+    ) +
+    add_p('第一条 （股权转让标的和转让价格）') +
+    add_p(`一、${get_zhuanrang_shourang_content().twoLine}`) +
+    add_p(`二、附属于股权的其他权利随股权的转让而转让。`) +
+    add_p(`三、受让方应于本协议签定之日起3日内，向出让方付清全部股权转让价款。`) +
+    add_p(`第二条 （承诺和保证）`) +
+    add_p(
+      `甲方保证本合同第一条转让给乙方的股权为甲方合法拥有，甲方拥有完全、有效的处分权。甲方保证其所转让的股权没有设置任何质押或其他担保权，不受任何第三人的追索。`
+    ) +
+    add_p(`第三条 （违约责任）`) +
+    add_p(`${add_underLine('违约责任')}`) +
+    add_p(`第四条 （解决争议的方法）`) +
+    add_p(`本协议受中华人民共和国相关法律的羁束并适用其解释。`) +
+    add_p(
+      `凡因本协议引起的或与本协议有关的任何争议，双方应友好协商解决。协商不成，应直接向人民法院起诉。`
+    ) +
+    add_p(`第五条 （其他）`) +
+    add_p(`一、本协议一式3份，协议各方各执1份，标的公司执1份，以备办理有关手续时使用。`) +
+    add_p(`二、本协议各方签字后生效。`) +
+    get_zhuanrang_shourang_content().qianzi +
+    add_date();
+
+  return all_content;
+}
+function get_zhuanrang_shourang_content(){
+  const zh_list = ['甲','乙','丙','丁',]
+  let count = 0
+  let content = ''
+  let churangfang = ''
+  let shourangfang = ''
+  let oneLine = ''
+  let twoLine =''
+  let qianzi = ''
+  for (let item of zhuanrang_info.zhuanrangs) {
+    churangfang += add_p(`${add_underLine(item.zhuanrang_name)}（以下称${zh_list[count]}方),住所:${add_underLine(`待填`)};`)
+    shourangfang += add_p(`${add_underLine(item.shourang_name)}（以下称${zh_list[count+1]}方),住所:${add_underLine(`待填`)};`)
+    oneLine+=`${zh_list[count]}方出资${get_chuzie_byName(item.zhuanrang_name)}万元人民币，占${get_chuzie_byName(item.zhuanrang_name) *100 / get_zhuceziben()}%；`
+    twoLine+= `${zh_list[count]}方将所持有标的公司${Number(item.zhuanrange) * 100 / get_zhuceziben()}％股权作价${item.zhuanrange}万元人民币转让给${zh_list[count+1]}方；`
+    qianzi+= add_qianzi(`${zh_list[count]}方：`) + add_qianzi(`     ${zh_list[count+1]}方：`)
+    count+=1
+  }
+  content = `${add_p('出让方:')+churangfang+add_p('受让方:')+shourangfang}`
+  return {content,oneLine,count,twoLine,qianzi}
+}
+function get_chuzie_byName(name:string){
+  return Number(formState_hezi.gudongs.filter(item=>item.name == name).map(item=>item.chuzie))
+}
+// 合资有限公司 获得总资本
+function get_zhuceziben() {
+  return formState_hezi.gudongs
+    .map((gudong) => Number(gudong.chuzie))
+    .reduce((pre, cur) => pre + cur);
+}
+
+function generate_tongyi() {
+  console.log(zhuanrang_info.zhuanrangs)
+  let content = '';
+  for (let item of zhuanrang_info.zhuanrangs) {
+    content += `同意股东${item.shourang_name} 受让${item.zhuanrang_name}持有的本公司${
+      (Number(item.zhuanrange) / get_zhuceziben()) * 100
+    }%的股权，`;
+  }
+  return content;
+}
+
+function generate_new_chuzibili() {
+  const modify_all_chuzi = modify_hezi.gudongs
+    .map((gudong) => Number(gudong.chuzie))
+    .reduce((pre, cur) => pre + cur);
+  let content = '';
+  for (let item of modify_hezi.gudongs) {
+    content += add_p(
+      `${add_underLine(item.name)},认缴出资额：${add_underLine(item.chuzie)}万元,出资比例${
+        (Number(item.chuzie) * 100) / modify_all_chuzi
+      } %`
+    );
+  }
+
+  return content;
+}
+// 一人有限公司股权转让章程修正案
 function generate_zhangchengxiuzhengan() {
   const all_content =
     add_title(`${formState.company_name}章程修正案`) +
@@ -263,7 +528,7 @@ function generate_guquanzhuanrang_xieyi() {
     add_p('第一条 （股权转让标的和转让价格）') +
     add_p(`一、甲方将所持有标的公司100％股权作价${formState.gudong_money}万元人民币转让给乙方。`) +
     add_p(`二、附属于股权的其他权利随股权的转让而转让。`) +
-    add_p(`三、受让方应于本协议签定之日起  日内，向出让方付清全部股权转让价款。`) +
+    add_p(`三、受让方应于本协议签定之日起3日内，向出让方付清全部股权转让价款。`) +
     add_p(`第二条 （承诺和保证）`) +
     add_p(
       `甲方保证本合同第一条转让给乙方的股权为甲方合法拥有，甲方拥有完全、有效的处分权。甲方保证其所转让的股权没有设置任何质押或其他担保权，不受任何第三人的追索。`
