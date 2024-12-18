99a64aa2264425c020a157d353030ff6a2b08dcb
yi <guiyc@outlook.com>
2024-12-18 09:18:57 +0800

    fix port

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server:{
-    host:'0.0.0.0',
-    port:5174,
-    proxy:{
-      '/api':{
-        target:'http://localhost:3000',
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
+      },
+    },
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
